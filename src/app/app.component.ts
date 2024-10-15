fe86baf6177728cddfa10c1c23f22a9508bdeb4e
aymenderbel <derbelaymen05@gmail.com>
2024-10-15 14:46:47 +0100

    navbar and sidebar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './layout/header/header.component';
+import { SideBarComponent } from './layout/side-bar/side-bar.component';
+import { NavBarComponent } from './layout/nav-bar/nav-bar.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet,HeaderComponent],
+  imports: [RouterOutlet,HeaderComponent, SideBarComponent , NavBarComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,19 @@
 module.exports = {
   content: ["./src/**/*.{html,ts}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        'roboto-flex': ['"Roboto Flex"', 'sans-serif'],
+      },
+      screens: {
+        'sm': '640px',   // Small devices (phones)
+        'md': '768px',   // Medium devices (tablets)
+        'lg': '1024px',  // Large devices (desktops)
+        'xl': '1120px',  // Extra large devices (larger desktops)
+        '2xl': '1230px', // 2X large devices (larger screens)
+      },
+      
+    },
   },
   plugins: [],
 };
