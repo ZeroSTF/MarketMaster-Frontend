07c22b8ef5f76e40f2149af465f1066cb8aa1a01
aymenderbel <derbelaymen05@gmail.com>
2024-10-17 11:43:18 +0100

    stock details component

--- src/app/features/dashboard/components/stockdetails/stockdetails.component.ts
+++ src/app/features/dashboard/components/stockdetails/stockdetails.component.ts
@@ -1,40 +1,32 @@
-import { Component, OnInit, signal } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { Component, inject} from '@angular/core';
+import { StockService } from '../../../../services/stock.service';
+import { CommonModule } from '@angular/common';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { MatPaginatorModule } from '@angular/material/paginator';
+import { MatSelectModule } from '@angular/material/select';
+import { MatSortModule } from '@angular/material/sort';
+import { MatTableModule } from '@angular/material/table';
 
 @Component({
   selector: 'app-stock-details',
   standalone: true,
-  imports: [],
+  imports: [
+    CommonModule,
+    MatTableModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatSelectModule,
+    MatIconModule,
+  ],
   templateUrl: './stockdetails.component.html',
   styleUrl: './stockdetails.component.css'
 })
-export class StockdetailsComponent implements OnInit {
-  stockInfo = signal({
-    symbol: '',
-    name: '',
-    price: 0,
-    change: 0,
-    marketCap: 0,
-    volume: 0,
-    peRatio: 0,
-    high52w: 0
-  });
+export class StockdetailsComponent {
+  private stockService = inject(StockService);
 
-  constructor(private route: ActivatedRoute) {}
-
-  ngOnInit() {
-    this.route.params.subscribe(params => {
-      
-      this.stockInfo.set({
-        symbol: params['symbol'],
-        name: 'Sample Stock Inc',
-        price: 406.32,
-        change: 2.24,
-        marketCap: 2800,
-        volume: 45.2,
-        peRatio: 28.5,
-        high52w: 425.32
-      });
-    });
-  }
+  // Signal to track the selected stock
+  selectedStock = this.stockService.selectedStockSignal;
+  
 }
