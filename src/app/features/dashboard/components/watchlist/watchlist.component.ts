6d72957f469aa384dd3369294a9b1e09cfc00bf1
aymenderbel <derbelaymen05@gmail.com>
2024-10-19 14:18:57 +0100

    updates for asset service and components logic

--- src/app/features/dashboard/components/watchlist/watchlist.component.ts
+++ src/app/features/dashboard/components/watchlist/watchlist.component.ts
@@ -1,19 +1,19 @@
 import { Component, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { AssetService } from '../../../../services/asset.service';
-import { WatchlistItem } from '../../../../models/asset.model';
 @Component({
   selector: 'app-watchListoverview',
   standalone: true,
   imports: [CommonModule],
-  templateUrl: './watchlistoverview.component.html',
-  styleUrls: ['./watchlistoverview.component.css']
+  templateUrl: './watchlist.component.html',
+  styleUrls: ['./watchlist.component.css']
 })
-export class WatchlistoverviewComponent {
+export class WatchlistComponent {
   private assetService = inject(AssetService);
 
   watchList = this.assetService.watchlist;
   bestWinners = this.assetService.bestWinners;
+  
 
   
 }
--- src/app/services/asset.service.ts
+++ src/app/services/asset.service.ts
@@ -1,12 +1,12 @@
 import { computed, Injectable, signal } from '@angular/core';
-import { AssetDiscover, WatchlistItem } from '../models/asset.model';
+import { AssetDiscover, AssetPortfolio, WatchlistItem } from '../models/asset.model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AssetService {
 
-  private assetsSignal =signal<AssetDiscover[]>  ([
+  private assetsSignal =signal<AssetDiscover[]| null>  ([
   { symbol: 'AAPL', price: 145.00, volume: 1000000, marketCap: 2400000000000, peRatio: 28.00, dividendYield: 0.58, sector: 'tech', trendDirection: 'up', logoUrl: 'images/logo1.png' },
   { symbol: 'MSFT', price: 300.00, volume: 800000, marketCap: 2250000000000, peRatio: 35.00, dividendYield: 0.82, sector: 'tech', trendDirection: 'down', logoUrl: 'images/logo2.png' },
   { symbol: 'GOOGL', price: 2800.00, volume: 500000, marketCap: 1900000000000, peRatio: 29.00, dividendYield: 0.00, sector: 'tech', trendDirection: 'up', logoUrl: 'images/logo3.png' },
@@ -27,7 +27,7 @@ export class AssetService {
   { symbol: 'TSLA', price: 700.00, volume: 600000, marketCap: 700000000000, peRatio: 200.00, dividendYield: 0.00, sector: 'auto', trendDirection: 'down', logoUrl: 'images/logo1.png' }
 
   ]);
-  private watchlistSignal = signal<WatchlistItem[]>([
+  private watchlistSignal = signal<WatchlistItem[]| null>([
     { symbol: 'CMPA', performance: 15.2, category: 'Tech', trend: 'up' },
     { symbol: 'CMPB', performance: 8.5, category: 'Finance', trend: 'down' },
     { symbol: 'CMPA', performance: 15.2, category: 'Tech', trend: 'up' },
@@ -38,19 +38,38 @@ export class AssetService {
     
     ]);
 
-  private bestWinnersSignal = signal<WatchlistItem[]>([
+  private bestWinnersSignal = signal<WatchlistItem[]| null>([
     { symbol: 'WNRA', performance: 18.7, category: 'Tech', trend: 'up' },
     { symbol: 'WNRB', performance: 14.3, category: 'Healthcare', trend: 'up' },
     { symbol: 'WNRA', performance: 18.7, category: 'Tech', trend: 'up' },
     ]);
 
+  private userAssetsSignal = signal<AssetPortfolio[] | null>([
+      { name: 'Apple Inc', symbol: 'AAPL', percentChange: 0.66, portfolioValue: 15215.70, trendImageUrl: 'images/upTrend.png' },
+      { name: 'Microsoft Corp', symbol: 'MSFT', percentChange: -0.32, portfolioValue: 12500.50, trendImageUrl: 'images/downTrend.png' },
+      { name: 'Amazon.com Inc', symbol: 'AMZN', percentChange: 1.25, portfolioValue: 9800.30, trendImageUrl: 'images/upTrend.png' },
+      { name: 'Alphabet Inc', symbol: 'GOOGL', percentChange: 0.88, portfolioValue: 11300.20, trendImageUrl: 'images/upTrend.png' },
+      { name: 'Meta Platforms Inc', symbol: 'META', percentChange: -0.75, portfolioValue: 8700.60, trendImageUrl: 'images/downTrend.png' },
+      { name: 'Tesla Inc', symbol: 'TSLA', percentChange: 2.10, portfolioValue: 7500.40, trendImageUrl: 'images/upTrend.png' },
+      { name: 'NVIDIA Corp', symbol: 'NVDA', percentChange: 1.50, portfolioValue: 10200.80, trendImageUrl: 'images/upTrend.png' },
+      { name: 'JPMorgan Chase & Co', symbol: 'JPM', percentChange: -0.20, portfolioValue: 6800.90, trendImageUrl: 'images/downTrend.png' },
+      { name: 'JPMorgan Chase & Co', symbol: 'JPM', percentChange: -0.20, portfolioValue: 6800.90, trendImageUrl: 'images/downTrend.png' },
+      { name: 'JPMorgan Chase & Co', symbol: 'JPM', percentChange: -0.20, portfolioValue: 6800.90, trendImageUrl: 'images/downTrend.png' },
+      { name: 'JPMorgan Chase & Co', symbol: 'JPM', percentChange: -0.20, portfolioValue: 6800.90, trendImageUrl: 'images/downTrend.png' },
+  
+    ])
+  selectedAssetSignal = signal<AssetDiscover | null>(null);
+
+
+
+  
   constructor() {}
 
-  selectedAssetSignal = signal<AssetDiscover | null>(null);
 
   readonly assets = computed(() => this.assetsSignal());
   readonly watchlist = computed(() => this.watchlistSignal());
   readonly bestWinners = computed(() => this.bestWinnersSignal());
+  readonly userAssets = computed(() => this.userAssetsSignal());
 
   getAssets() {
     return this.assetsSignal();
