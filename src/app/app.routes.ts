667e3bfdcccce5bce8071c1a1986ccb397a148fb
Koussaykouki <koussay.kouki@esprit.tn>
2024-10-22 17:21:58 +0100

    updates

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,15 +1,12 @@
 import { Routes } from '@angular/router';
-import { DashboardOverviewComponent } from './features/dashboard/pages/dashboard-overview/dashboard-overview.component';
-import { DashboardExploreComponent } from './features/dashboard/pages/dashboard-explore/dashboard-explore.component';
+import { OverviewComponent } from './features/dashboard/pages/overview/overview.component';
+import { DiscoverComponent } from './features/dashboard/pages/discover/discover.component';
 import { DashboardLayoutComponent } from './layout/dashboard-layout/dashboard-layout.component';
-import { DashboardPortfolioComponent } from './features/dashboard/pages/dashboard-portfolio/dashboard-portfolio.component';
-import { DashboardSettingsComponent } from './features/dashboard/pages/dashboard-settings/dashboard-settings.component';
-import { TraderLayoutComponent } from './layout/trader-layout/trader-layout.component';
-import { LearningLayoutComponent } from './layout/learning-layout/learning-layout.component';
-import { LearningOverviewComponent } from './features/learning/pages/learning-overview/learning-overview.component';
-import { LearningCalendarComponent } from './features/learning/pages/learning-calendar/learning-calendar.component';
+import { PortfolioComponent } from './features/dashboard/pages/portfolio/portfolio.component';
 import { GamesLayoutComponent } from './layout/games-layout/games-layout.component';
 import { GameOverviewComponent } from './features/games/pages/game-overview/game-overview.component';
+import { SettingsComponent } from './features/dashboard/pages/settings/settings.component';
+import { TraderLayoutComponent } from './layout/trader-layout/trader-layout.component';
 
 export const routes: Routes = [
   {
@@ -44,25 +41,24 @@ export const routes: Routes = [
       },
       {
         path: 'overview',
-        component: DashboardOverviewComponent,
+        component: OverviewComponent,
       },
 
       {
         path: 'discover',
-        component: DashboardExploreComponent,
+        component: DiscoverComponent,
       },
       {
         path: 'portfolio',
-        component: DashboardPortfolioComponent,
+        component: PortfolioComponent,
       },
       {
         path: 'settings',
-        component: DashboardSettingsComponent,
+        component: SettingsComponent,
       },
     ],
   },
 
-  // TRADER ROUTES
   {
     path: 'trader',
     component: TraderLayoutComponent,
@@ -73,38 +69,15 @@ export const routes: Routes = [
         pathMatch: 'full',
       },
     ],
-  },
-
-  // LEARNING ROUTES
-  {
-    path: 'learning',
-    component: LearningLayoutComponent,
-    children: [
-      {
-        path: '',
-        redirectTo: 'overview',
-        pathMatch: 'full',
-      },
-      {
-        path: 'overview',
-        component: LearningOverviewComponent,
-      },
-      {
-        path: 'calendar',
-        component: LearningCalendarComponent,
-      },
-    ],
-  },
-  {
-    path: 'game',
+  },{
+    path: 'gamer',
     component: GamesLayoutComponent,
     children: [
       {
         path: 'overview',
         component: GameOverviewComponent,
-      }
-  
-      
+      },
     ],
   },
+  
 ];
