d84ffd36dd401345b980b38153360ada2f11ae45
aymenderbel <derbelaymen05@gmail.com>
2024-10-22 18:36:21 +0100

    first commit today, overview from dashbiard

--- src/app/features/learning/pages/learning-overview/learning-overview.component.ts
+++ src/app/features/learning/pages/learning-overview/learning-overview.component.ts
@@ -1,9 +1,15 @@
 import { Component } from '@angular/core';
+import { OverviewdetailsComponent } from '../../../dashboard/components/overviewdetails/overviewdetails.component';
+import { CoursComponent } from '../../../dashboard/components/cours/cours.component';
+import { NewsComponent } from '../../../dashboard/components/news/news.component';
+import { WatchlistComponent } from '../../../dashboard/components/watchlist/watchlist.component';
+import { ChartComponent } from '../../../dashboard/components/chart/chart.component';
+import { TradesoverviewComponent } from '../../../dashboard/components/tradesoverview/tradesoverview.component';
 
 @Component({
   selector: 'app-learning-overview',
   standalone: true,
-  imports: [],
+  imports: [OverviewdetailsComponent,CoursComponent, NewsComponent,WatchlistComponent,ChartComponent,TradesoverviewComponent],
   templateUrl: './learning-overview.component.html',
   styleUrl: './learning-overview.component.css'
 })
