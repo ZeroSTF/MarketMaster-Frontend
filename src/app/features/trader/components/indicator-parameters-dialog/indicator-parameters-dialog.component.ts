841a53436c2bb7beef5778e935be3c7e237188aa
ZeroSTF <35609287+ZeroSTF@users.noreply.github.com>
2024-12-02 17:52:32 +0100

    indicator params dialog darkmode

--- src/app/features/trader/components/indicator-parameters-dialog/indicator-parameters-dialog.component.ts
+++ src/app/features/trader/components/indicator-parameters-dialog/indicator-parameters-dialog.component.ts
@@ -27,39 +27,63 @@ import { CommonModule } from '@angular/common';
     MatButtonModule,
   ],
   template: `
-    <h2 mat-dialog-title>{{ data.indicator.name }} Parameters</h2>
-    <mat-dialog-content>
-      <form [formGroup]="parametersForm">
-        <mat-form-field
-          *ngFor="let param of data.currentParameters"
-          class="w-full"
+    <div
+      class="p-6 bg-white dark:!bg-gray-800 text-gray-900 dark:!text-gray-100 rounded-lg"
+    >
+      <h2 class="text-xl font-semibold mb-4 dark!:text-white">
+        {{ data.indicator.name }} Parameters
+      </h2>
+      <mat-dialog-content>
+        <form [formGroup]="parametersForm" class="space-y-4">
+          <mat-form-field
+            *ngFor="let param of data.currentParameters"
+            class="w-full"
+            appearance="outline"
+          >
+            <mat-label>{{ param.name }}</mat-label>
+            <input
+              matInput
+              type="number"
+              [formControlName]="param.name"
+              [min]="param.min"
+              [max]="param.max"
+              [step]="param.step || 1"
+              class="dark:!bg-gray-700 dark:!text-gray-100"
+            />
+            <mat-hint> Range: {{ param.min }} - {{ param.max }} </mat-hint>
+            <mat-error *ngIf="parametersForm.get(param.name)?.invalid">
+              Value must be between {{ param.min }} and {{ param.max }}
+            </mat-error>
+          </mat-form-field>
+        </form>
+      </mat-dialog-content>
+      <mat-dialog-actions class="flex justify-end space-x-2 mt-4">
+        <button
+          mat-stroked-button
+          (click)="onCancel()"
+          class="dark:!text-gray-100 dark:!border-gray-600"
         >
-          <mat-label>{{ param.name }}</mat-label>
-          <input
-            matInput
-            type="number"
-            [formControlName]="param.name"
-            [min]="param.min"
-            [max]="param.max"
-          />
-          <mat-error *ngIf="parametersForm.get(param.name)?.invalid">
-            Value must be between {{ param.min }} and {{ param.max }}
-          </mat-error>
-        </mat-form-field>
-      </form>
-    </mat-dialog-content>
-    <mat-dialog-actions>
-      <button mat-button (click)="onCancel()">Cancel</button>
-      <button
-        mat-raised-button
-        color="primary"
-        (click)="onSave()"
-        [disabled]="parametersForm.invalid"
-      >
-        Save
-      </button>
-    </mat-dialog-actions>
+          Cancel
+        </button>
+        <button
+          mat-raised-button
+          color="primary"
+          (click)="onSave()"
+          [disabled]="parametersForm.invalid"
+          class="dark:!bg-primary-600 dark:!text-white"
+        >
+          Save
+        </button>
+      </mat-dialog-actions>
+    </div>
   `,
+  styles: [
+    `
+      ::ng-deep .mat-mdc-form-field-subscript-wrapper {
+        @apply dark:text-gray-400;
+      }
+    `,
+  ],
 })
 export class IndicatorParametersDialogComponent {
   parametersForm: FormGroup;
