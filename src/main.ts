e7bed56fa10500d04899972cdcbe6f6f523a498c
ZeroSTF <35609287+ZeroSTF@users.noreply.github.com>
2025-01-01 22:18:59 +0100

    fixes

--- src/main.ts
+++ src/main.ts
@@ -5,19 +5,15 @@ import { importProvidersFrom } from '@angular/core';
 import { StoreModule } from '@ngrx/store';
 import { EffectsModule } from '@ngrx/effects';
 import { StoreDevtoolsModule } from '@ngrx/store-devtools';
-import { gameReducer } from './app/store/actions/game.reducer';  // Adjust path to your reducer
-import { GameEffects } from './app/store/actions/game.effects';  // Adjust path to your effects
-import 'zone.js'; // Ensure Zone.js is imported
-import { HttpClientModule } from '@angular/common/http';
+import { gameReducer } from './app/store/actions/game.reducer';
+import { GameEffects } from './app/store/actions/game.effects';
 
 bootstrapApplication(AppComponent, {
   providers: [
     importProvidersFrom(
       StoreModule.forRoot({ game: gameReducer }),  // Configure your root store
       EffectsModule.forRoot([GameEffects]),  // Include the effects
-      StoreDevtoolsModule.instrument({ maxAge: 25, logOnly: false }),
-      HttpClientModule,
-      
+      StoreDevtoolsModule.instrument({ maxAge: 25, logOnly: false }),      
     ),
     ...appConfig.providers
   ]
